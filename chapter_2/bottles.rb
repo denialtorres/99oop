23bdf5608591293bc6ddaca0e52ebca5ae650aa5
Daniel Torres <denial.torres@gmail.com>
2021-11-07 15:49:35 -0600

    cover all the possible combinations of bottles from 99 to 0

--- chapter_2/bottles.rb
+++ chapter_2/bottles.rb
@@ -2,7 +2,18 @@
 
 class Bottles
   def verse(number)
-    if number == 2
+    case number
+    when 0
+      "No more bottles of milk on the wall, " +
+      "no more bottles of milk.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of milk on the wall.\n"
+    when 1
+      "1 bottle of milk on the wall, " +
+      "1 bottle of milk.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of milk on the wall.\n"
+    when 2
       "2 bottles of milk on the wall, " +
       "2 bottles of milk.\n" +
       "Take one down and pass it around, " +

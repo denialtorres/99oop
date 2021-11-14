c17169b641c012335c5112abaac7274f5904b411
Daniel Torres <denial.torres@gmail.com>
2021-11-14 14:33:58 -0600

    chapter 4 template

---
+++ chapter_4/bottles.rb
@@ -0,0 +1,49 @@
+# Shameless Green
+
+class Bottles
+  def song
+    verses(99, 0)
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).collect do |number_of_bottles|
+      verse(number_of_bottles)
+    end.join("\n")
+  end
+
+  def pronoun(number)
+    if number == 1
+      'it'
+    else
+      'one'
+    end
+  end
+
+  def container(number)
+    if number == 1
+      'bottle'
+    else
+      'bottles'
+    end
+  end
+
+  def verse(number)
+    case number
+    when 0
+      'No more bottles of milk on the wall, ' \
+        "no more bottles of milk.\n" \
+        'Go to the store and buy some more, ' \
+        "99 bottles of milk on the wall.\n"
+    when 1
+      "#{number} #{container(number)} of milk on the wall, " \
+        "#{number} #{container(number)} of milk.\n" \
+        "Take #{pronoun(number)} down and pass it around, " \
+        "no more bottles of milk on the wall.\n"
+    else
+      "#{number} #{container(number)} of milk on the wall, " \
+        "#{number} #{container(number)} of milk.\n" \
+        "Take #{pronoun(number)} down and pass it around, " \
+        "#{number - 1} #{container(number - 1)} of milk on the wall.\n"
+    end
+  end
+end

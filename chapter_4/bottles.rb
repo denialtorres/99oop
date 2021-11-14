f64c556e13af21a91320162e1a6931c24233e757
Daniel Torres <denial.torres@gmail.com>
2021-11-14 16:13:02 -0600

    0 case send container

--- chapter_4/bottles.rb
+++ chapter_4/bottles.rb
@@ -11,6 +11,14 @@ class Bottles
     end.join("\n")
   end
 
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
   def pronoun(number)
     if number == 1
       'it'
@@ -27,23 +35,15 @@ class Bottles
     end
   end
 
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number
-    end
-  end
-
   def verse(number)
     case number
     when 0
-      'No more bottles of milk on the wall, ' \
+      "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " \
         "no more bottles of milk.\n" \
         'Go to the store and buy some more, ' \
         "99 bottles of milk on the wall.\n"
     else
-      "#{number} #{container(number)} of milk on the wall, " \
+      "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " \
         "#{number} #{container(number)} of milk.\n" \
         "Take #{pronoun(number)} down and pass it around, " \
         "#{quantity(number-1)} #{container(number - 1)} of milk on the wall.\n"

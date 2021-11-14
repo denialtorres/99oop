745cebc7815f9830eac4b884085ac5cb62e0e59d
Daniel Torres <denial.torres@gmail.com>
2021-11-14 14:53:55 -0600

    pronoun and quantity methods

--- chapter_4/bottles.rb
+++ chapter_4/bottles.rb
@@ -27,6 +27,14 @@ class Bottles
     end
   end
 
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number
+    end
+  end
+
   def verse(number)
     case number
     when 0
@@ -34,16 +42,11 @@ class Bottles
         "no more bottles of milk.\n" \
         'Go to the store and buy some more, ' \
         "99 bottles of milk on the wall.\n"
-    when 1
-      "#{number} #{container(number)} of milk on the wall, " \
-        "#{number} #{container(number)} of milk.\n" \
-        "Take #{pronoun(number)} down and pass it around, " \
-        "no more bottles of milk on the wall.\n"
     else
       "#{number} #{container(number)} of milk on the wall, " \
         "#{number} #{container(number)} of milk.\n" \
         "Take #{pronoun(number)} down and pass it around, " \
-        "#{number - 1} #{container(number - 1)} of milk on the wall.\n"
+        "#{quantity(number-1)} #{container(number - 1)} of milk on the wall.\n"
     end
   end
 end
